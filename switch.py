dc55b65b1682bf79a9fd3e5c3deb4ffac4c70af0
zunaisha zaryab <zunaishazaryab@gmail.com>
2025-09-27 00:21:43 -0700

    first commit

---
+++ switch.py
@@ -0,0 +1,29 @@
+day=input("enter a day of week:")
+match day.lower():
+    case "monday":
+        print("today is Monday")
+    case "tuesday":
+        print("today is Tuesday")
+    case "wednesday":
+        print("today is Wednesday")
+    case "thursday":
+        print("today is Thursday")
+    case "friday":
+        print("today is Friday")
+    case "saturday":
+        print("today is Saturday")
+    case "sunday":
+        print("today is Sunday")
+    case _:
+        print("ínvalid day")
+
+
+
+name=["zunaisha", "zaryab"]
+for n in name:
+    print(n)
+
+name=["zunaisha", "zaryab"]
+print(name)
+
+
